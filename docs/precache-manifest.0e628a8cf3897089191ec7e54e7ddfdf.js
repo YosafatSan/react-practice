95abac77bd243d509784a90f18231c7f6933b298
Yosafat Sanchez Leon <yosafat.sanchez@bigcommerce.com>
2020-07-23 09:57:59 -0500

    Added build files

---
+++ docs/precache-manifest.0e628a8cf3897089191ec7e54e7ddfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff1d2551153f218b823d8b282213638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08667986d2d67160f87c",
+    "url": "/static/css/main.e2dcadcc.chunk.css"
+  },
+  {
+    "revision": "12bde6be097fb9059c39",
+    "url": "/static/js/2.e4cfaa2f.chunk.js"
+  },
+  {
+    "revision": "332d064cfa0fea1b7f4c0024f5237aeb",
+    "url": "/static/js/2.e4cfaa2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08667986d2d67160f87c",
+    "url": "/static/js/main.2b34344e.chunk.js"
+  },
+  {
+    "revision": "318b0f48921573befd6b",
+    "url": "/static/js/runtime-main.f88d9363.js"
+  }
+]);
