d84fe6a76c9bd5b9b83715ee03093010cd48bc59
Yosafat Sanchez Leon <yosafat.sanchez@bigcommerce.com>
2020-07-23 12:12:18 -0500

    Added new build files

---
+++ docs/precache-manifest.0b8f0a69fac8e16ba32b29eb06b3ccf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179ebf8a418ecc5bf88f153d45230321",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "086ce81dac206f99b628",
+    "url": "/react-practice/static/css/main.e2dcadcc.chunk.css"
+  },
+  {
+    "revision": "12bde6be097fb9059c39",
+    "url": "/react-practice/static/js/2.e4cfaa2f.chunk.js"
+  },
+  {
+    "revision": "332d064cfa0fea1b7f4c0024f5237aeb",
+    "url": "/react-practice/static/js/2.e4cfaa2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086ce81dac206f99b628",
+    "url": "/react-practice/static/js/main.07c33d50.chunk.js"
+  },
+  {
+    "revision": "02631561f96414f97f45",
+    "url": "/react-practice/static/js/runtime-main.c4c9416c.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e628a8cf3897089191ec7e54e7ddfdf.js"
+  "/react-practice/precache-manifest.0b8f0a69fac8e16ba32b29eb06b3ccf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-practice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
